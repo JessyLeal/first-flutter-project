6d4f5b5fbaec84443d8e12b9ccbb388c7eab9b24
Jéssica Leal <62772815+Jessy777-cripto@users.noreply.github.com>
2022-07-12 13:59:11 -0300

    creating a stateful widget

--- Tópico 1/statup_namer/lib/main.dart
+++ Tópico 1/statup_namer/lib/main.dart
@@ -17,14 +17,25 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Welcome to Flutter'),
         ),
-        body: Center(                          // Drop the const, and
-          //child: Text('Hello World'),        // Replace this text...
-          child: Text(
-            wordPair.asPascalCase,
-            style: TextStyle(fontSize: 50),
-            ),  // With this text.
+        body: Center(
+          child: RandomWords(),
         ),
       ),
     );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
+
+  @override
+  State<RandomWords> createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
   }
 }
