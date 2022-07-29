d9b8eb08c7ffe7f53133c18b452cd9a5db95f11f
Jéssica Leal <62772815+Jessy777-cripto@users.noreply.github.com>
2022-07-29 11:44:09 -0300

    feat(main.dart): button and coluns inserted

--- Tópico 2/statup_namer/lib/main.dart
+++ Tópico 2/statup_namer/lib/main.dart
@@ -3,6 +3,8 @@ import 'dart:ui';
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
+enum ViewType { grid, list }
+
 void main() {
   runApp(const MyApp());
 }
@@ -30,6 +32,8 @@ class RandomWords extends StatefulWidget {
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
   final _biggerFont = const TextStyle(fontSize: 18);
+  ViewType _viewType = ViewType.list;
+  int _colum = 1;
 
   @override
   Widget build(BuildContext context) {
@@ -37,15 +41,32 @@ class _RandomWordsState extends State<RandomWords> {
       appBar: AppBar(
         title: Text('Statup Name Genaretor'),
       ),
+      floatingActionButton: FloatingActionButton(
+          child:
+              Icon(_viewType == ViewType.grid ? Icons.grid_view : Icons.list),
+          onPressed: () {
+            if (_viewType == ViewType.grid) {
+              _viewType = ViewType.list;
+              _colum = 1;
+            } else {
+              _viewType = ViewType.grid;
+              _colum = 2;
+            }
+            setState(() {});
+          }),
       body: _buildSuggestions(),
     );
   }
 
   Widget _buildSuggestions() {
-    return ListView.builder(
+    return GridView.builder(
       padding: const EdgeInsets.all(16.0),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: _colum,
+        childAspectRatio: _viewType == ViewType.grid ? 1 : 10,
+      ),
       itemBuilder: (context, i) {
-        if (i.isOdd) {
+        if (i.isOdd && _viewType == ViewType.list) {
           return const Divider();
         }
 
@@ -57,13 +78,24 @@ class _RandomWordsState extends State<RandomWords> {
       },
     );
   }
-}
 
-Widget _buildRow(WordPair pair) {
-  return ListTile(
-    title: Text(
-      pair.asPascalCase,
-      // style: _biggerFont,
-    ),
-  );
+  Widget _buildRow(WordPair pair) {
+    if (_viewType == ViewType.grid) {
+      return Card(
+        margin: const EdgeInsets.all(12),
+        child: Center(
+            child: Text(
+          pair.asPascalCase,
+          style: _biggerFont,
+        )),
+      );
+    } else {
+      return ListTile(
+        title: Text(
+          pair.asPascalCase,
+          // style: _biggerFont,
+        ),
+      );
+    }
+  }
 }
